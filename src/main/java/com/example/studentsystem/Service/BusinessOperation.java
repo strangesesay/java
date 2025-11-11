929b7153f1ef57cbd683749dc17084e3e07f94d0
strangesesay <126430875+strangesesay@users.noreply.github.com>
2025-11-11 03:58:11 +0000

    Delete src directory

--- src/main/java/com/example/studentsystem/Service/BusinessOperation.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentsystem.Service;
-
-import com.example.studentsystem.Model.Student;
-
-public interface BusinessOperation {
-    Student enrollStudentInCourse(Long studentId, Long courseId);
-}
