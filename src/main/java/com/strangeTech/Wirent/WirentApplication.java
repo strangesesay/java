cc145b4de666ecb35c6cbc488d83b55050f2e883
strangesesay <126430875+strangesesay@users.noreply.github.com>
2025-11-11 10:05:27 +0000

    fiesrt

---
+++ src/main/java/com/strangeTech/Wirent/WirentApplication.java
@@ -0,0 +1,13 @@
+package com.strangeTech.Wirent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WirentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strangeTech/Wirent/WirentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strangeTech.Wirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
